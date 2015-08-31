201bd6e0f23341a66059b0b77d1dc8c4234d0561
Hugo Torres <hugo@Hugos-MacBook-Pro.local>
2015-08-31 11:20:36 -0400

    Simplify maybe

--- maybe.js
+++ maybe.js
@@ -19,23 +19,6 @@ var Maybe = function(value) {
 Maybe.prototype.andThen = function(func) {
 	var args = Array.prototype.slice.call(arguments);
 
-	if (args.length > 1) {
-		if (typeof args[0] !== 'function') {
-			console.log(args[0]);
-			return;
-		}
-
-		if (this._value === null) {
-			console.log('in value check');
-			return;
-		}
-		
-		return new Maybe(func.call(
-			this._value,
-			Array.prototype.slice.call(args, 1)
-		));
-	}
-
 	if (this._value === null) {
 		//TODO: handle 'nothing' case
 		return new Maybe("This maybe object is undefined").
